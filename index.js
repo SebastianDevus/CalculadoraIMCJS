5d13863d15eeb31a1ccf2382e9f063421f3eaa41
etec adolpho berezin <sebastiandevus@gmail.com>
2022-10-31 20:41:39 -0300

    Adiciona mais script

--- index.js
+++ index.js
@@ -17,7 +17,46 @@ campoMassa.addEventListener("change", event => {
 });
 
 botao.addEventListener("click", event => {
-    let paragrafoExibeIMC = getElementById("exibeIMC");
+    const paragrafoExibeIMC = document.getElementById("exibeIMC");
+    const paragrafoExibeSituacao = document.getElementById("exibeSituacao");
     
     const valorIMC = (dadosUsuario.massa / (dadosUsuario.altura ** 2)).toFixed(2);
+    const situacaoUsuario = null;
+
+    switch (key) {
+        case valorIMC < 17:
+            situacaoUsuario = "Muito abaixo do peso.";
+            break;
+
+        case valorIMC >= 17 && valorIMC < 18.5:
+            situacaoUsuario = "Abaixo do peso.";
+            break;
+
+        case valorIMC >= 18.5 && valorIMC < 24.5:
+            situacaoUsuario = "Peso normal."
+            break;
+
+        case valorIMC >= 25 && valorIMC < 30:
+            situacaoUsuario = "Acima do peso."
+            break;
+
+        case valorIMC >= 30 && valorIMC < 35:
+            situacaoUsuario = "Obesidade."
+            break;
+
+        case valorIMC >= 35 && valorIMC < 40:
+            situacaoUsuario = "Obesidade severa."
+            break
+
+        case valorIMC >= 40:
+            situacaoUsuario = "Obesidade mórbida."
+            break;
+
+        default:
+            console.error("Um erro ocorreu na verificação da situação do usuário.");
+            break;
+    }
+
+    paragrafoExibeIMC.innerHTML = `${valorIMC} kg/m².`;
+    paragrafoExibeSituacao.innerHTML = `${situacaoUsuario}.`;
 })
